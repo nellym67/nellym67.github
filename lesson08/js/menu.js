05948c76b739547e3ae97b3342a6fc991b036041
nellym67 <ram14020@byui.edu>
2021-06-24 02:07:51 -0600

    week08

---
+++ lesson08/js/menu.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation');
+
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')},false);
+
+    
+});
