d1f1d4eb1a7e57510b25833994e7ff8e57cea5d7
nellym67 <ram14020@byui.edu>
2021-06-25 23:18:03 -0600

    lazy img

--- lesson05/js copy/menu.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-    const hambutton = document.querySelector('.ham');
-    const mainnav = document.querySelector('#navigation');
-
-    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')},false);
-    
-});
