f8204676a68423b851bec85fd93855ea8a33ddc3
nellym67 <ram14020@byui.edu>
2021-07-01 13:39:28 -0600

    prof[phets

--- lesson09/js/towns.js
+++ lesson09/js/towns.js
@@ -15,32 +15,45 @@ fetch(requestURL)
     
     for(let i=0; 1< filter_town.length; i++){
         let body = document.createElement('section');
-        let h2 = document.createElement("h2");
+        body.classList.add("town");
+        body.innerHTML=`<h2>${filter_town.name}<h2>
+        <h3>${filter_town.motto}</h3>
+        <p>Year Founded: ${filter_town[i].yearFounded}</p>
+        <p>Population: ${filter_town[i].currentPopulation}</p>
+        <p>Annual Rain Fall: ${filter_town[i].averageRainfall}</p>
+        `;
+        // let h2 = document.createElement("h2");
         let h3 = document.createElement("h3");
-        let p1 = document.createElement("p");
-        let p2 = document.createElement("p");
-        let p3 = document.createElement("p");
+        // let p1 = document.createElement("p");
+        // let p2 = document.createElement("p");
+        // let p3 = document.createElement("p");
         let image = document.createElement("img");
 
         h3.setAttribute("class","json_h3");
         body.setAttribute("class","json_section");
-        p1.setAttribute("class","data");
-        p2.setAttribute("class","data");
-        p3.setAttribute("class","data");
-        h2.textContent= filter_town[i].name;
-        h3.textContent=filter_town[i].motto;
-        p1.textContent="Year Founded: " + filter_town[i].yearFounded;
-        p2.textContent="Population: " + filter_town[i].currentPopulation;
-        p3.textContent="Annual Rain Fall: " + filter_town[i].averageRainfall;
+
+
+        // h2.classList.add("data");
+        // h3.classList.add("data");
+        // p1.classList.add("data");
+        // p2.classList.add("data");
+        // p3.classList.add("data");
+        // h2.textContent= filter_town[i].name;
+        // h3.textContent=filter_town[i].motto;
+        // p1.textContent="Year Founded: " + filter_town[i].yearFounded;
+        // p2.textContent="Population: " + filter_town[i].currentPopulation;
+        // p3.textContent="Annual Rain Fall: " + filter_town[i].averageRainfall;
         image.setAttribute('src', "images/" + filter_town[i].photo);
         
-        document.querySelector('div#body').appendChild(body);
+        
 
-        body.appendChild(h2);
-        body.appendChild(h3);
-        body.appendChild(p1);
-        body.appendChild(p2);
-        body.appendChild(p3);
+        // body.appendChild(h2);
+        // body.appendChild(h3);
+        // body.appendChild(p1);
+        // body.appendChild(p2);
+        // body.appendChild(p3);
+
+        document.querySelector('div#body').appendChild(body);
         body.appendChild(image);
 
     }
