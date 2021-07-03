674b9d50a94cef163e02ff9ba35f7475acedc89a
nellym67 <ram14020@byui.edu>
2021-07-03 00:13:13 -0600

    week09

---
+++ lesson09/js/windchill.js
@@ -0,0 +1,16 @@
+
+let temperature = parseInt(document.getElementById("temperature").innerText);
+let wind_speed = parseInt(document.getElementById("wind_speed").innerText);
+let math_pow=Math.pow(wind_speed,0.16);
+let wind_chill = 35.74+(0.6215*temperature)-35.75*math_pow+(0.4275*(temperature*math_pow));
+
+
+
+if (temperature<=50 && wind_speed > 3){
+    document.getElementById("wind").innerHTML = wind_chill.toFixed() + " °F";
+}
+
+else{
+
+document.getElementById("wind").innerHTML = "N/A"
+}
